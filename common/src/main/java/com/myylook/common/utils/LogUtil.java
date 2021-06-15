9c5ab88bc6e95e13063eb6d948d6f4e68e79648b
838245284 <838245284@qq.com>
2021-06-15 12:57:55 +0800

    commit log

--- common/src/main/java/com/myylook/common/utils/LogUtil.java
+++ common/src/main/java/com/myylook/common/utils/LogUtil.java
@@ -1,6 +1,7 @@
 package com.myylook.common.utils;
 
 import android.text.TextUtils;
+import android.util.Log;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -29,6 +30,25 @@ public class LogUtil {
             } catch (Exception e1) {
                 e1.printStackTrace();
             }
+        }
+    }
+
+    public static void e(String tag, String msg) {
+        if (tag == null || tag.length() == 0
+                || msg == null || msg.length() == 0)
+            return;
+
+        int segmentSize = 3 * 1024;
+        long length = msg.length();
+        if (length <= segmentSize ) {// 长度小于等于限制直接打印
+            Log.e(tag, msg);
+        }else {
+            while (msg.length() > segmentSize ) {// 循环分段打印日志
+                String logContent = msg.substring(0, segmentSize );
+                msg = msg.replace(logContent, "");
+                Log.e(tag, logContent);
+            }
+            Log.e(tag, msg);// 打印剩余日志
         }
     }
 }
